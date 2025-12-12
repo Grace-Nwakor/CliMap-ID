347feb74b5bdc5bebd70cb9774bbeab0d1e5ea1b
Grace-Nwakor <nwakorgracygold@gmail.com>
2025-12-12 19:01:21 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h2>CliMap ID Platform</h2>
+      <p>React frontend is connected.</p>
+    </div>
+  );
+}
+
+export default App;
