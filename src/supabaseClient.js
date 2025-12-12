128869bfed636b256904999df2b1d4dae81ca2a6
Grace-Nwakor <nwakorgracygold@gmail.com>
2025-12-12 20:01:13 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://tfhzcztkccpqlukvttbv.supabase.co";
-const supabaseKey = "sb_publishable_AIIDJhYTrHxDllivzuM4MA_P5FQUr5D";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
