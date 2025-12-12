f62965b8892aa187cdc1ce40204cb0b644440d85
Grace-Nwakor <nwakorgracygold@gmail.com>
2025-12-12 20:14:39 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+// In Vite, we use import.meta.env instead of process.env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
